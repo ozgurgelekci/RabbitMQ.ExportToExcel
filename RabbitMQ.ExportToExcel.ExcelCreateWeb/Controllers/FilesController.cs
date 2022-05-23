9dda7be2a3c7289e805195e5783dcb9eab839816
ozgurgelekci <ozgurgelekci@gmail.com>
2022-05-23 17:05:38 +0300

    files controller

---
+++ RabbitMQ.ExportToExcel.ExcelCreateWeb/Controllers/FilesController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RabbitMQ.ExportToExcel.ExcelCreateWeb.Contexts;
+using System.IO;
+
+namespace RabbitMQ.ExportToExcel.ExcelCreateWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilesController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public FilesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Upload(IFormFile file, int fileId)
+        {
+            if (file is not { Length: > 0 })
+            {
+                return BadRequest();
+            }
+
+            var userFile = await _context.UserFiles.FirstAsync(x => x.Id == fileId);
+
+            var filePath = userFile.FileName + Path.GetExtension(file.FileName);
+
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/files", filePath);
+
+            using FileStream stream = new(path, FileMode.Create);
+
+            await file.CopyToAsync(stream);
+
+            userFile.CreatedDate = DateTime.Now;
+
+            userFile.FilePath = filePath;
+
+            userFile.FileStatus = Entities.FileStatus.Completed;
+
+            await _context.SaveChangesAsync();
+
+            return Ok();
+
+        }
+    }
+}
